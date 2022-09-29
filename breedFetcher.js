0d7ee57fcefcd2e8fb6d981ee66888e4a0ccca03
William Gadd <wgadd@sasktel.net>
2022-09-29 20:48:47 +0000

    Added support for command line input and implimented error 404, also handling receiving an empty object and not finding cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,39 @@
 const request = require('request');
-const getCatBreedInformation = function(breed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, message, body) => {
+const getCatBreedInformation = function(breed, callback) {
+  request(`https://api.thcatapi.com/v1/breeds/search?q=${breed}`, (error, message, body) => {
   
     if (error) {
       console.log("error:", error);
-    }  
-    // console.log("message: ", message);
-    const breedInfo = JSON.parse(body)[0]; 
-    // console.log("typeof(body)", typeof(body))
-    // console.log("body", body);
-    // console.log("breedInfo:", breedInfo);
-    console.log(breedInfo.description);
-  });
-}
+      callback("Cat API Access failed:");
+      return;
+    }
+    
+    console.log(message.statusCode);
+    if (message.statusCode === 404) {
+      callback("Error 404: Page not found");
+      return;
+    }
 
-getCatBreedInformation('siam');
+    const infoArray = JSON.parse(body);
+    
+    /* console.log(body);
+    console.log("status code ", message.statusCode); */
+    
+    if (!(Array.isArray(infoArray))) {
+      callback("Request Failed");
+      return;
+    }
+    
+    if (infoArray.length === 0) {
+      callback("Cat Breed not found");
+      return;
+    }
+    const breedInfo = JSON.parse(body)[0];
+    callback(breedInfo.description);
+    // console.log("breedInfo:", breedInfo);
+  });
+};
+
+const arg = process.argv[2];
+
+getCatBreedInformation(arg, description => console.log(description));
