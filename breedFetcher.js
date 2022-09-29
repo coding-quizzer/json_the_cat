2b4300dbd4b58d37d6892bc2ba512e4f6403e83a
William Gadd <wgadd@sasktel.net>
2022-09-29 19:58:21 +0000

    Made function called the getCatBreedInformation that so far prints out information about the siberian breed acessed from the cats api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
 const request = require('request');
+const getCatBreedInformation = function() {
+  request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, message, body) => {
+  
+    if (error) {
+      console.log("error:", error);
+    }  
+    // console.log("message: ", message);
+    const breedInfo = JSON.parse(body)[0]; 
+    // console.log("typeof(body)", typeof(body))
+    // console.log("body", body);
+    // console.log("breedInfo:", breedInfo);
+    console.log(breedInfo.description);
+  });
+}
+
+getCatBreedInformation('siberian');
