f340938b44b1226d49629274ab986c675c0805cd
William Gadd <wgadd@sasktel.net>
2022-09-29 21:15:06 +0000

    Changed callback function two have two parameters, an error parameter if it fails and a description parameter if it succeeds

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 const request = require('request');
 const getCatBreedInformation = function(breed, callback) {
-  request(`https://api.thcatapi.com/v1/breeds/search?q=${breed}`, (error, message, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, message, body) => {
   
     if (error) {
       console.log("error:", error);
-      callback("Cat API Access failed:");
+      callback("Cat API Access failed:", null);
       return;
     }
     
     console.log(message.statusCode);
     if (message.statusCode === 404) {
-      callback("Error 404: Page not found");
+      callback("Error 404: Page not found", null);
       return;
     }
 
@@ -19,21 +19,21 @@ const getCatBreedInformation = function(breed, callback) {
     /* console.log(body);
     console.log("status code ", message.statusCode); */
     
-    if (!(Array.isArray(infoArray))) {
-      callback("Request Failed");
+    if (!(Array.isArray(infoArray))){
+      callback("Request Failed", null);
       return;
     }
     
     if (infoArray.length === 0) {
-      callback("Cat Breed not found");
+      callback("Cat Breed not found", null);
       return;
     }
     const breedInfo = JSON.parse(body)[0];
-    callback(breedInfo.description);
+    callback(null, breedInfo.description);
     // console.log("breedInfo:", breedInfo);
   });
 };
 
 const arg = process.argv[2];
 
-getCatBreedInformation(arg, description => console.log(description));
+getCatBreedInformation(arg, (error, description) => console.log(error ? error : description));
